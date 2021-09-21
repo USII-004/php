7ec0f7c4018e995e857832f7483af9f27402fbde
OLAKUNLE <stephenkunle26@gmail.com>
2021-09-22 00:24:23 +0100

    please check

---
+++ calculator.php
@@ -0,0 +1,10 @@
+<h1>Mortagage calculator</h1>
+
+<form action ='mortgagecalc.php' method ='POST'>
+
+<p>Enter Principal:</br><input type='text' name='' value=''  placeholder='principal'></p></br>
+
+<p>Time:</br><input type='text' name='' value=''  placeholder='time'></p></br>
+
+<button type='submit'>calculate</button>
+</form>
---
+++ mortgagecalc.php
@@ -0,0 +1,83 @@
+/*
+Write a function that is able to calculate a mortgage loan using the following:
+
+Loans from 5,000 - 9,999 uses 2.5% Interest rate
+
+Loans from 10,000 - 19,999 uses 3.5% Interest rate
+
+Loans from 20,000 - 49,999, uses 4% Interest rate
+
+Loans from 50,000 and above, uses 6% Interest rate
+
+Formula for calculation:
+
+Simple Interest= Principal x Rate x Time ÷ (100 x 12)
+
+Amount Payable = Principal + simpleInterest */
+
+
+
+
+
+ <?php
+
+ loan = '$loan'
+ simpleInterest = '$simpleInterest';
+ Principal = '$principal';
+ Time = '$time';
+ Amount = '$amount';
+
+ if ('$loan' >= 5000 && '$loan' <= 9999) {
+
+     function loans('$principal','$time'){
+         '$simpleInterest' = '$principal' * 2.5 * '$time' / (100 * 12) ;
+
+         echo return '$simpleInterest';
+     };
+
+    };
+
+     elseif('$loan' >= 10000 && "$loan" <= 19999) {
+
+            function loans('$principal','$time'){
+                '$simpleInterest' = '$principal' * 3.5 * '$time' / (100 * 12) ;
+
+               echo return '$simpleInterest';
+
+         };
+
+        };
+
+     elseif('$loan' >= 20000 && '$loan' <= 49999){
+
+        function loans('$principal','$time'){
+            '$simpleInterest' = '$principal' * 4 * '$time' / (100 * 12) ;
+
+            echo return '$simpleInterest';
+
+
+     };
+
+    };
+
+     else('loan' >= 50000){
+
+        function loans('$principal','$time'){
+            '$simpleInterest' = '$principal' * 6 * '$time' / (100 * 12) ;
+
+            echo return '$simpleInterest';
+
+     };
+
+    };
+
+
+
+ function cash('$principal', '$simpleInterest'){
+
+     echo '$amount' = '$principal' + '$simpleInterest';
+
+     return '$amount';
+
+    };
+
